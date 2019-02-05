a3964a62193e305bede72165d122fa7c077f172e
FahadAlMutairi5 <almteref@gmail.com>
2019-02-05 12:27:51 +0300

    Project Tow

--- actions.py
+++ actions.py
@@ -2,7 +2,7 @@
 from data import stores
 from components import Cart
 
-site_name = ""  # Give your site a name
+site_name = "TawaiqSite"  # Give your site a name
 
 def welcome():
     print("Welcome to %s\nFeel free to shop throughout the stores we have, and only checkout once!" % site_name)
@@ -12,18 +12,36 @@ def print_stores():
     prints the list of stores in a nice readable format.
     """
     # your code goes here!
+    for item in stores:
+        print ("Ths store name is %s" % item.name)
 
 def get_store(store_name):
     """
     receives a name for a store, and returns the store object with that name.
     """
     # your code goes here!
+    for item in stores:
+        if item.name == store_name:
+            return item
+    return False
 
 def pick_store():
     """
     prints list of stores and prompts user to pick a store.
     """
     # your code goes here!
+    print_stores()
+    pickstore = input("Pick a store by typing its name Or type 'checkout' to pay your bills:")
+    if pickstore == "checkout":
+        return "checkout"
+    else:
+        stor = get_store(pickstore)
+        if stor != False:
+            return stor
+        else:
+            print ("No store with that name. Please try again:")
+            pick_store()
+
 
 def pick_products(cart, picked_store):
     """
@@ -31,11 +49,33 @@ def pick_products(cart, picked_store):
     """
     # your code goes here!
 
+    picked_store.print_products()
+    user_input_product = input ("Pick the items you'd like to add in your cart by typing the product name Or type 'Back' to go back to the main menu:")
+    while user_input_product != "back":
+        for item in picked_store.products:
+            if item.name == user_input_product:
+                cart.add_to_cart(item)
+        user_input_product = input("Type another products Or 'back' to main menu:")
+    
+    
 def shop():
     """
     The main shopping functionality
     """
+    
     cart = Cart()
+    pickstore = True
+    while pickstore:
+        store = pick_store()
+        if store == "checkout":
+            pickstore = False
+        else:
+            pick_products(cart,store)
+    cart.checkout()
+    
+        
+    
+    
     # your code goes here!
 
 def thank_you():
--- components.py
+++ components.py
@@ -1,22 +1,30 @@
 # CLASSES AND METHODS
 class Store():
+    
+    
     def __init__(self, name):
         """
         Initializes a new store with a name.
         """
         # your code goes here!
+        self.name = name
+        self.products = []
 
     def add_product(self, product):
         """
         Adds a product to the list of products in this store.
         """
         # your code goes here!
+        self.products.append(product)
 
     def print_products(self):
         """
         Prints all the products of this store in a nice readable format.
         """
         # your code goes here!
+        for product in self.products:
+            print (product)
+            
 
 
 class Product():
@@ -25,38 +33,62 @@ class Product():
         Initializes a new product with a name, a description, and a price.
         """
         # your code goes here!
+        self.name = name
+        self.description = description
+        self.price = price
 
     def __str__(self):
         # your code goes here!
+        return "(%s, %s, %s)" % (self.name, self.description, self.price)
 
 
 class Cart():
+
+    
     def __init__(self):
         """
         Initializes a new cart with an empty list of products.
         """
         # your code goes here!
+        self.products = []
 
     def add_to_cart(self, product):
         """
         Adds a product to this cart.
         """
         # your code goes here!
+        self.products.append(product)
 
     def get_total_price(self):
         """
         Returns the total price of all the products in this cart.
         """
         # your code goes here!
+        total = 0
+        for item in self.products:
+            total += item.price
+        return total
+
 
     def print_receipt(self):
         """
         Prints the receipt in a nice readable format.
         """
         # your code goes here!
+        for item in self.products:
+            print (item.name)
+            print (self.get_total_price())
 
     def checkout(self):
         """
         Does the checkout.
         """
         # your code goes here!
+        
+        self.print_receipt()
+        prompt = input("Tho order ok? ")
+        if prompt == "ok":
+            print ("Your order has been placed.")
+        else:
+            print ("Your order has been cancelled.")
+
